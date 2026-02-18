6b3789c616d5027f2b63ea87ab8ba7f45b2b30bd
Sokoris1 <nivokbu@gmail.com>
2026-02-18 23:31:49 +0900

    style change
    header footer minor style change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,22 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 export const Footer = () => {
   return (
     <>
-      <footer className='flex text-xl bg-[#00051c] text-white font-mono p-4 pl-40 pr-40 pt-15 pb-15'>
-        <div className='italic'>
+      <footer className='flex text-base bg-[#00051c] text-white font-mono p-4 pl-40 pr-40 pt-15 pb-15'>
+        <div className='italic text-xl'>
           <a>Genius is the world’s biggest collection of song lyrics and musical knowledge</a>
 
         </div>
         <div className='flex ml-auto'>
           <ul className='space-y-2 pr-4'>
-            <li><Link to="/policy" className='text-white hover:text-blue-400 transition'>Политика конфиденциальности</Link></li>
-            <li><Link to="/terms" className='text-white hover:text-blue-400 transition'>Условия пользования</Link></li>
-            <li><Link to="/contacts" className='text-white hover:text-blue-400 transition'>Контакты</Link></li>
+            <li><Link to="/policy" className='text-white underline-offset-4 decoration-gray-500 hover:underline'>Политика конфиденциальности</Link></li>
+            <li><Link to="/terms" className='text-white underline-offset-4 decoration-gray-500 hover:underline'>Условия пользования</Link></li>
+            <li><Link to="/contacts" className='text-white underline-offset-4 decoration-gray-500 hover:underline'>Контакты</Link></li>
             <li className='text-sm text-gray-600'>review @ 2026</li>
           </ul>
           <ul className='space-y-2'>
-            <li><Link to="/Auth" className='text-white hover:text-blue-400 transition'>Войти</Link></li>
-            <li><Link to="/About" className='text-white hover:text-blue-400 transition'>О нас</Link></li>
-            <li><Link to="/" className='text-white hover:text-blue-400 transition'>Главная</Link></li>
+            <li><Link to="/Auth" className='text-white underline-offset-4 decoration-gray-500 hover:underline'>Войти</Link></li>
+            <li><Link to="/About" className='text-white underline-offset-4 decoration-gray-500 hover:underline'>О нас</Link></li>
+            <li><Link to="/" className='text-white underline-offset-4 decoration-gray-500 hover:underline'>Главная</Link></li>
           </ul>
         </div>
         
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,19 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 export const Header = () => {
   return (
     <>
-      <header className='pl-40 flex text-xl bg-gradient-to-r from-[#3500D3] to-[#3910b3] text-white font-mono fixed top-0 w-screen'>
+      <header className='pl-40 flex text-base bg-gradient-to-r from-[#3500D3] to-[#3910b3] text-white font-mono fixed top-0 w-screen'>
         <div className='ml-4   p-4 italic'>
             <a className=''><Link to="/" className='text-white hover:text-blue-200 transition'>Reviews</Link></a>
         </div>
         <div className='p-4'>
           <nav className='space-x-5'>
-            <Link to="/" className='text-white hover:text-blue-400 transition'>Главная</Link>
-            <Link to="/reviews" className='text-white hover:text-blue-400 transition'>Рецензии</Link>
+            <Link to="/" className='text-white underline-offset-4 hover:underline '>Главная</Link>
+            <Link to="/reviews" className='text-white underline-offset-4 hover:underline'>Рецензии</Link>
           </nav>
         </div>
         
         <div className='mr-4 pr-40 p-4 absolute inset-y-0 right-0'>
-          <a className='text-white hover:text-blue-400 transition'>
+          <a className='text-white underline-offset-4 hover:underline'>
             <Link to="/auth">Авторизация</Link>
           </a>
         </div>
