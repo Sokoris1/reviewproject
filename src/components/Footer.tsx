f3dca27dd5f0161a8721cff2e91999bc4c2d4392
Sokoris1 <nivokbu@gmail.com>
2026-02-26 10:38:36 +0900

    26/02/26 edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom'
 export const Footer = () => {
   return (
     <>
-      <footer className='flex bg-[#17000f] text-white font-comfortaa p-4 pl-40 pr-40 pt-15 pb-15'>
+      <footer className='flex bg-black text-white font-comfortaa p-4 pl-40 pr-40 pt-15 pb-15'>
         <div className='italic text-lg w-2xl '>
-          <a className='font-bold'>harmonix - платформа для тех, кто не равнодушен к музыке и ко всему, что с этим связано. </a>
+          <a className='font-bold block'>harmon<span className="text-[#8400FF]">ix</span> - платформа для тех, кто не равнодушен к музыке и ко всему, что с этим связано. </a>
+          <a className='text-sm text-gray-600'>harmonix @ 2026</a>
         </div>
         <div className='flex ml-auto text-sm'>
           <ul className='space-y-2 pr-4'>
-            <li><Link to="/policy" className='text-white underline-offset-4 decoration-[#78014d] hover:underline'>политика конфиденциальности</Link></li>
-            <li><Link to="/terms" className='text-white underline-offset-4 decoration-[#78014d] hover:underline'>условия пользования</Link></li>
-            <li><Link to="/contacts" className='text-white underline-offset-4 decoration-[#78014d] hover:underline'>контакты</Link></li>
+            <li><Link to="/policy" className='text-white underline-offset-4 decoration-[#8400FF] hover:underline'>политика конфиденциальности</Link></li>
+            <li><Link to="/terms" className='text-white underline-offset-4 decoration-[#8400FF] hover:underline'>условия пользования</Link></li>
+            <li><Link to="/contacts" className='text-white underline-offset-4 decoration-[#8400FF] hover:underline'>контакты</Link></li>
           </ul>
           <ul className='space-y-2'>
-            <li><Link to="/Auth" className='text-white underline-offset-4 decoration-[#78014d] hover:underline'>войти</Link></li>
-            <li><Link to="/About" className='text-white underline-offset-4 decoration-[#78014d] hover:underline'>о нас</Link></li>
-            <li><Link to="/reviews" className='text-white underline-offset-4 decoration-[#78014d] hover:underline'>статьи</Link></li>
-            <li className='text-sm text-gray-600'>review @ 2026</li>
+            <li><Link to="/auth" className='text-white underline-offset-4 decoration-[#8400FF] hover:underline'>войти</Link></li>
+            <li><Link to="/about" className='text-white underline-offset-4 decoration-[#8400FF] hover:underline'>о нас</Link></li>
+            <li><Link to="/reviews" className='text-white underline-offset-4 decoration-[#8400FF] hover:underline'>статьи</Link></li>
           </ul>
         </div>
         
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,11 @@ export const Header = () => {
     <>
       <header className='pl-40 flex  bg-white shadow-[0_4px_18.9px_-3px_rgba(0,0,0,0.25)] fixed top-0 w-screen'>
         <div className='ml-4 p-4'>
-            <a className=''><Link to="/" className='font-bold font-comfortaa text-2xl'>harmonix</Link></a>
+            <a className=''>
+              <Link to="/" className='font-bold font-comfortaa text-2xl'>
+                harmon<span className="text-[#8400FF]">ix</span>
+              </Link>
+            </a>
         </div>
         <div className='p-4 pl-20 text-lg'>
           <nav className='space-x-5'>
@@ -22,24 +26,6 @@ export const Header = () => {
         </div>
 
       </header>
-      {/* <header className='pl-40 flex text-base bg-[#611c3d] text-white font-mono fixed top-0 w-screen'>
-        <div className='ml-4   p-4 italic'>
-            <a className=''><Link to="/" className='text-white hover:text-red-200 transition'>Reviews</Link></a>
-        </div>
-        <div className='p-4'>
-          <nav className='space-x-5'>
-            <Link to="/" className='text-white underline-offset-4 hover:underline '>Главная</Link>
-            <Link to="/reviews" className='text-white underline-offset-4 hover:underline'>Рецензии</Link>
-          </nav>
-        </div>
-        
-        <div className='mr-4 pr-40 p-4 absolute inset-y-0 right-0'>
-          <a className='text-white underline-offset-4 hover:underline'>
-            <Link to="/auth">Авторизация</Link>
-          </a>
-        </div>
-
-      </header> */}
         
     </>
   )
